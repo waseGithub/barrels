7a1093dbdea19cfcf88dfc90d74ffaabc5ed460b
waseGithub <Harvey>
2022-10-04 17:35:11 +0100

    combined temperature

--- tipper_logging/tipper_logging.ino
+++ tipper_logging/tipper_logging.ino
@@ -1,80 +1,65 @@
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
-//
-//
-//
-//
-//#define ONE_WIRE_BUS 3
-//OneWire oneWire(ONE_WIRE_BUS);
-//DallasTemperature sensors(&oneWire);
-//
-//
-//int barrel_id = 1;
-//
-//
-////latest version of gastippercode as of 10-09-2021
-//const int  R1TipperPin = 14;  // digital in 2 (pin the reactor 1's tipper is attached to)
-//int R1TipperCounter = 0;
-//int R1TipperState = 1;
-//int lastR1TipperState = 1;
-//unsigned long previousMillis = 0;
-//const long interval = 3000; //milliseconds between each print of number of tips (1000ms = 1 second), 1800000 = 30 mins
-//
-//
-//
-//
-//
-//
-//void setup() {
-//  //initialize serial communication at 9600 bits per second:
-//  Serial.begin(9600);
-//  pinMode(R1TipperPin, INPUT_PULLUP);
-//  while (!Serial);
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//void loop() {
-////  add_id();
-////   
-//  // read the switch input pins:
-//  R1TipperState = digitalRead(R1TipperPin);
-//  // compare the switchState to its previous state
-//  if (R1TipperState != lastR1TipperState) {
-//    if (R1TipperState == HIGH) {
-//      R1TipperCounter+=1;
-//    }
-//  }
-// 
-//
-//  lastR1TipperState = R1TipperState;
-//
-//
-//
-//  unsigned long currentMillis = millis();
-//  if (currentMillis - previousMillis >= interval) {
-//  sensors.requestTemperatures(); 
-//  Serial.print(barrel_id);
-//  Serial.print(",");
-//  Serial.print(sensors.getTempCByIndex(0));
-//  Serial.print(",");
-//  Serial.println(R1TipperCounter);
-//  previousMillis = currentMillis;
-//  }
-//
-//  
-//  delay(10);
-//
-//  
-//
-// 
-//
-//
-//
-//
-//}
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 3
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+int barrel_id = 2;
+int deviceCount = 0;
+float tempC;
+const int  R1TipperPin = 14;  // digital in 2 (pin the reactor 1's tipper is attached to)
+int R1TipperCounter = 0;
+int R1TipperState = 1;
+int lastR1TipperState = 1;
+unsigned long previousMillis = 0;
+const long interval = 3000; //milliseconds between each print of number of tips (1000ms = 1 second), 1800000 = 30 mins
+
+void setup(void)
+{
+  sensors.begin();  // Start up the library
+  Serial.begin(9600);
+  pinMode(R1TipperPin, INPUT_PULLUP);
+  deviceCount = sensors.getDeviceCount();
+}
+
+void loop(void)
+{ 
+
+    R1TipperState = digitalRead(R1TipperPin);
+  // compare the switchState to its previous state
+  if (R1TipperState != lastR1TipperState) {
+    if (R1TipperState == HIGH) {
+      R1TipperCounter+=1;
+    }
+  }
+  lastR1TipperState = R1TipperState;
+
+
+
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+  Serial.print(barrel_id);
+  Serial.print(",");
+  sensors.requestTemperatures(); 
+  
+  // Display temperature from each sensor
+  for (int i = 0;  i < deviceCount;  i++)
+  {
+    tempC = sensors.getTempCByIndex(i);
+    Serial.print(tempC);
+    Serial.print(",");
+  }
+  Serial.println(R1TipperCounter);
+  previousMillis = currentMillis;
+  }
+  
+
+  delay(10);
+}
