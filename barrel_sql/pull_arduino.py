f58d4a69922f9d1e56178e8cb3a52a5ab8526ccd
harveyrutland <hcrutland@mail.com>
2023-05-05 16:49:11 +0100

    again

--- barrel_sql/pull_arduino.py
+++ barrel_sql/pull_arduino.py
@@ -5,8 +5,6 @@ import csv
 import os
 import pandas as pd
 import numpy
-import subprocess
-import datetime as datetime
 
 
 import serial.tools.list_ports
@@ -20,7 +18,7 @@ drive = GoogleDrive(gauth)
 import re
 import subprocess
 import pandas as pd 
-
+#import dictpy
 
 device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
 df = subprocess.check_output("lsusb")
@@ -30,7 +28,7 @@ line1 = None
 line2 = None
 line3 = None 
 line4 = None 
-line5 = None
+
 
 
 
@@ -52,7 +50,7 @@ import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
 
 Megas = []
-unos = []
+
 for port, desc, hwid in sorted(ports):
         print("{}: {} [{}]".format(port, desc, hwid))
         if '2341:0042' in hwid:
@@ -60,9 +58,9 @@ for port, desc, hwid in sorted(ports):
           print(port)
           Megas.append(port)
         elif '2341:0042' in hwid:
-          print('Requested device found mega 2')
-          print(port)
-          Megas.append(port)
+         print('Requested device found mega 2')
+         print(port)
+         Megas.append(port)
         elif '2341:0042' in hwid:
           print('Requested device found mega 3')
           print(port)
@@ -71,49 +69,57 @@ for port, desc, hwid in sorted(ports):
           print('Requested device found mega 4')
           print(port)
           Megas.append(port)
-        elif '2341:0043' in hwid:
-          print('Requested device found uno 1')
+
+        elif '2341:0042' in hwid:
+          print('Requested device found mega 3')
           print(port)
-          unos.append(port)
+          Megas.append(port)
+        elif '2341:0042' in hwid:
+          print('Requested device found mega 4')
+          print(port)
+          Megas.append(port)
+
+      
           
 print('Megas as port:')          
 print(Megas)
-print('Unos as port:')          
-print(unos)
+
          
            
 
-ser1 = serial.Serial(str(Megas[0]),  38400, timeout = 25)
-ser2 = serial.Serial(str(Megas[1]),  38400, timeout = 25)
-ser3 = serial.Serial(str(Megas[2]),  38400, timeout = 25)
-ser4 = serial.Serial(str(Megas[3]),  38400, timeout = 25)
-#ser5 = serial.Serial(str(unos[0]),  38400, timeout = 25)
+ser1 = serial.Serial(str(Megas[0]),  9600, timeout = 25)
+ser2 = serial.Serial(str(Megas[1]),  9600, timeout = 25)
+ser3 = serial.Serial(str(Megas[2]),  9600, timeout = 25)
+ser4 = serial.Serial(str(Megas[3]),  9600, timeout = 25)
 print("channels correct")
     
 time.sleep(5)
 
 if __name__ == '__main__':
-   
     
    
-   
-    
-   
-    
+
     ser1.flush()
     ser2.flush()
     ser3.flush()
     ser4.flush()
 
+
+    i = 0
+
+   
     while True:
-         
-      
+         ser1.flush()
+         ser2.flush()
+         ser3.flush()
+         ser4.flush()
+         print('sql script')
+         i +=1
+         print('Current count =')
+         print(i)
          try:
-            print('logging')
-
-
             if ser1.in_waiting > 0:
-            
+                
                 line1 = ser1.readline().decode("utf-8")
                 
                 
@@ -121,6 +127,7 @@ if __name__ == '__main__':
                     
                     writer = csv.writer(f, delimiter=",")
                     writer.writerow([time.asctime(),line1])
+
                     
 
             if ser2.in_waiting > 0:
@@ -156,17 +163,18 @@ if __name__ == '__main__':
                     writer = csv.writer(f, delimiter=",")
                     writer.writerow([time.asctime(),line4])
             
-            time.sleep(10)
-            print('writing gas data')
-            print(line1)
+           
+            print('writing data')
+            print(Megas[0])
+            print(line1) 
+            print(Megas[1])
             print(line2)
+            print(Megas[2])
             print(line3)
+            print(Megas[3])
             print(line4)
 
          except UnicodeDecodeError:
-             pass
+
                 
- 
-               
-               
-       
+  
