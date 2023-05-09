7c482fce9e88fb2dd4862b4f8fb53aba56f0d063
harveyrutland <hcrutland@mail.com>
2023-05-09 10:56:25 +0100

    again

--- barrel_sql/pushtosql_flow.py
+++ barrel_sql/pushtosql_flow.py
@@ -84,19 +84,19 @@ data_gas['datetime'] = data_gas['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
 print(data_gas.columns)
 
-# cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='Barrels_datasets')
-# cursor = cnx.cursor()
-# cols = "`,`".join([str(i) for i in data_gas.columns.tolist()])
-# for i,row in data_gas.iterrows():
-#     sql = "INSERT INTO `flowmeter_temperature` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-#     cursor.execute(sql, tuple(row))
-#     cnx.commit()
+cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='Barrels_datasets')
+cursor = cnx.cursor()
+cols = "`,`".join([str(i) for i in data_gas.columns.tolist()])
+for i,row in data_gas.iterrows():
+    sql = "INSERT INTO `flowmeter_temperature` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+    cursor.execute(sql, tuple(row))
+    cnx.commit()
 
 
 
-# cnx.close()
+cnx.close()
 
-# print('pushed')
+print('pushed')
 
 # os.remove('Sensor_A.csv')
 # os.remove('ensor_B.csv')
