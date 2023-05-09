8bb1012a5d23e8571dd68401bcaddf06813b5398
harveyrutland <hcrutland@mail.com>
2023-05-09 09:59:29 +0100

    again

--- barrel_sql/pushtosql_flow.py
+++ barrel_sql/pushtosql_flow.py
@@ -33,9 +33,6 @@ def replace_negative_with_zero(value):
 
 
 
-
-
-
 data_gas = pd.DataFrame()
 data_tank = pd.DataFrame()
 upload_file_list = ['/home/waselab2/Documents/barrels/barrel_sql/Sensor_A.csv','/home/waselab2/Documents/barrels/barrel_sql/Sensor_B.csv', '/home/waselab2/Documents/barrels/barrel_sql/Sensor_C.csv','/home/waselab2/Documents/barrels/barrel_sql/Sensor_D.csv',]
@@ -58,33 +55,19 @@ for upload_file in upload_file_list:
     
 data_gas = data_gas.drop(columns=data_gas.columns[0])
 data_gas.columns =['ID','Temp1','Temp2','Cnt']
-
-
-
-
 data_gas = data_gas.sort_values(by='ID')
-
-
-
-
-
-
 
 
 
 data_gas = data_gas[data_gas['ID'].isin(['1', '2', '3', '4'])]
 data_gas = data_gas.apply(pd.to_numeric)
+
+
 data_gas.reset_index(inplace=True)
 data_gas.set_index(['datetime', 'ID'], inplace=True)
 data_gas = data_gas.applymap(replace_string_with_zero)
 data_gas = data_gas.applymap(replace_negative_with_zero)
-
-
 data_gas['Cnt'] = data_gas.groupby(level='ID')['Cnt'].resample('30T', level=0).max().fillna(0)
-
-# data_gas = data_gas.groupby(level='ID').resample('30T', level=0).max()
-
-
 
 
 
@@ -94,17 +77,8 @@ data_gas['datetime'] = data_gas['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
 
 
-print(data_gas)
-
-
-
-
 
 cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='Barrels_datasets')
-
-
- 
-
 cursor = cnx.cursor()
 cols = "`,`".join([str(i) for i in data_gas.columns.tolist()])
 for i,row in data_gas.iterrows():
@@ -112,7 +86,6 @@ for i,row in data_gas.iterrows():
     cursor.execute(sql, tuple(row))
     cnx.commit()
 
-# os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
 
 
 cnx.close()
