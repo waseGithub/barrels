7dc026a91a8ce57ab63d3af588961173fb5911ab
harveyrutland <hcrutland@mail.com>
2023-06-05 12:41:04 +0100

    again

--- barrel_sql/checkpy.py
+++ barrel_sql/checkpy.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+import psutil
 import subprocess
 
 script_path = "/home/waselab2/Documents/barrels/barrel_sql/pull_arduino.py"
 
-process_check_cmd = f"pgrep -fl 'python3 {script_path}'"
+is_running = False
 
-process_output = subprocess.check_output(process_check_cmd, shell=True)
-process_output = process_output.decode().strip()
+for proc in psutil.process_iter(["cmdline"]):
+    if proc.info["cmdline"] and proc.info["cmdline"][0] == "python3" and script_path in proc.info["cmdline"]:
+        is_running = True
+        break
 
-if any(script_path in line for line in process_output.split("\n")):
+if is_running:
     print("Script is already running.")
 else:
     print("Script is not running. Starting...")
