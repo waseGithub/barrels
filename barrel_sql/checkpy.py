c395753b8d2c9c91050eef08cf5320bd676dd846
harveyrutland <hcrutland@mail.com>
2023-06-05 12:38:18 +0100

    again

--- barrel_sql/checkpy.py
+++ barrel_sql/checkpy.py
@@ -4,14 +4,13 @@ import subprocess
 
 script_path = "/home/waselab2/Documents/barrels/barrel_sql/pull_arduino.py"
 
-process_check_cmd = f"pgrep -f 'python3 {script_path}'"
+process_check_cmd = f"pgrep -fl 'python3 {script_path}'"
 
 process_output = subprocess.check_output(process_check_cmd, shell=True)
 process_output = process_output.decode().strip()
 
-if process_output:
-    script_pid = process_output.split("\n")[0]
-    print(f"Script is already running with PID: {script_pid}")
+if any(script_path in line for line in process_output.split("\n")):
+    print("Script is already running.")
 else:
     print("Script is not running. Starting...")
     subprocess.Popen(["x-terminal-emulator", "-e", "python3", script_path])
